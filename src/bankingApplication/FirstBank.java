13adb540b4ec69c283021dfc2b92d75fad58ae73
Scuba Steve <stevengart@gmail.com>
2022-01-27 09:29:26 -0800

    First Commit

---
+++ src/bankingApplication/FirstBank.java
@@ -0,0 +1,17 @@
+package bankingApplication;
+
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account steve = new Account("Steve Gartner", "A00001");
+		
+		
+		Account fred = new Account("Fred", "B00002");
+		fred.showMenu();
+		
+		
+	}
+
+}
